2025112de3c5686546fde52f7ecaa424b74b30e6
mini801107 <mini801107@yahoo.com.tw>
2017-02-19 14:58:34 +0800

    recorder function

--- Mianbot/ChatRoomViewController.swift
+++ Mianbot/ChatRoomViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import Speech
 import Foundation
 import RxSwift
 import Alamofire
@@ -15,8 +16,7 @@ import SwiftyJSON
 import JSQMessagesViewController
 
 
-
-class ChatRoomViewController: JSQMessagesViewController, buttonActionDelegate {
+class ChatRoomViewController: JSQMessagesViewController, SFSpeechRecognizerDelegate, buttonActionDelegate {
     
     let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: UIColor(red: 10/255, green:180/255, blue: 230/255, alpha: 1.0))
     let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.lightGray)
@@ -27,6 +27,11 @@ class ChatRoomViewController: JSQMessagesViewController, buttonActionDelegate {
     var isMessageWithButton = [Bool]()
     var linkInReply = [String]()
     var linkKeyword = [String]()
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "zh-TW"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,9 +53,39 @@ class ChatRoomViewController: JSQMessagesViewController, buttonActionDelegate {
             print("token = \(self.token)")
         }
         
-        
+        //Customize toolbar
         self.incomingCellIdentifier = CustomCellWithButtonsIncomingCell.cellReuseIdentifier()
         self.collectionView!.register(CustomCellWithButtonsIncomingCell.nib(), forCellWithReuseIdentifier: CustomCellWithButtonsIncomingCell.cellReuseIdentifier())
+    
+        self.inputToolbar.contentView.rightBarButtonItemWidth = 50
+        self.inputToolbar.contentView.leftBarButtonItemWidth = 30
+        self.inputToolbar.contentView.leftBarButtonItem.setImage(UIImage(named: "microphone"), for: .normal)
+        self.inputToolbar.contentView.leftBarButtonItem.addTarget(self, action: #selector(recorderTapped), for: .touchUpInside)
+        
+        //Authorize the speech recognizer
+        self.inputToolbar.contentView.leftBarButtonItem.isEnabled = false
+        speechRecognizer?.delegate = self
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            var isButtonEnabled = false
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.inputToolbar.contentView.leftBarButtonItem.isEnabled = isButtonEnabled
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -149,6 +184,95 @@ class ChatRoomViewController: JSQMessagesViewController, buttonActionDelegate {
     override func didPressAccessoryButton(_ sender: UIButton!) {
         
     }
+    
+    // MARK :- Recorder function
+    var textFromVoice: String = ""
+    func recorderTapped(sender: UIButton!) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            self.inputToolbar.contentView.leftBarButtonItem.isEnabled = false
+            self.inputToolbar.contentView.rightBarButtonItem.isEnabled = true
+            self.inputToolbar.contentView.textView.text.append(self.textFromVoice)
+            print("Stop Recording")
+        } else {
+            self.textFromVoice = ""
+            startRecording()
+            print("Start Recording")
+        }
+    }
+    
+    func startRecording() {
+        //Check if recognitionTask is running. If so, cancel the task and the recognition
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        //Create an AVAudioSession to prepare for the audio recording.
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        //Check if the audioEngine (your device) has an audio input for recording.
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        //Check if the recognitionRequest object is instantiated and is not nil.
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        //Tell recognitionRequest to report partial results of speech recognition as the user speaks.
+        recognitionRequest.shouldReportPartialResults = true
+        //Start the recognition by calling the recognitionTask method of our speechRecognizer.
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            if result != nil {
+                self.textFromVoice = (result?.bestTranscription.formattedString)!
+                isFinal = (result?.isFinal)!
+            }
+            
+            //If there is no error or the result is final, stop the audioEngine (audio input) and stop the recognitionRequest and recognitionTask.
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                self.inputToolbar.contentView.leftBarButtonItem.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        //textView.text = "Say something, I'm listening!"
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            self.inputToolbar.contentView.leftBarButtonItem.isEnabled = true
+        } else {
+            self.inputToolbar.contentView.leftBarButtonItem.isEnabled = false
+        }
+    }
+
     
 }
 
@@ -306,7 +430,8 @@ extension ChatRoomViewController {
         reloadMessagesView()
         replyMessage(incomingMessage: message!)
     }
-   
+    
+
 }
 
 
